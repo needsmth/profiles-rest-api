9e9cd7cc35f4b9ccbd9c6fefe8615b966085fbc0
leonidgoldshtein <needsmth@gmail.com>
2020-05-13 18:40:45 +0300

    Added our first viewset and router

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -23,7 +23,7 @@ class UserProfileManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, name, password):
-        """Creates and saves a new superuser with given detailes. """
+        """Creates and saves a new superuser with given detailes."""
 
         user = self.create_user(email, name, password)
 
